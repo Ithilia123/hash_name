88af94eaf3571f4132859a9eb67d134c6a4de860
Mpampakas <91561616+Mpampakas@users.noreply.github.com>
2021-09-28 23:18:48 +0300

    Updated Main
    i have to create a public fanction called hash and create a new public variable to the hash_name class called msize.

--- class.cpp
+++ class.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <cstring>
 #include "class.h"
+using namespace std;
 
-void hash_name::EraseLetters()
+int main()
 {
-    int c = strlen(name),j=0;
+    cout << "Please write your first name : " ;
+    hash_name a;
+    cin>>a.name;
+    cout<<endl<<"Please write the max size of the integer that the hash can return (>0 & <1.000.000.000): ";
+    cin>>a.msize;
 
-    for (int i=0;i<c;i++)
-    {
-        if(name[i]>=65 && name[i]<=90)
-        {
-            name[j]=name[i]+32;
-            j++;
-        }
-        else if(name[i]>=97 && name[i]<=122)
-            {
-                name[j++]=name[i];
-            }
-    }
-    name[j]='\0';
+    a.EraseLetters();
+    cout<<endl<<"The hash of the name "<<"'"<<a.name<<"'"<<" that is smaller than: "<<a.msize<<" is : "<<a.Hash()<<endl<<endl;
+
+
+    return 0;
 }
